16bff30ba949d2ec3f10a540dc9f08d06e61ab94
afaa423 <18786089980@163.com>
2020-12-25 14:01:50 +0800

    第二次上传作业

--- text/src/day02/Test07.java
+++ text/src/day02/Test07.java
@@ -16,7 +16,35 @@ public class Test07 {
         Scanner console = new Scanner(System.in);
         System.out.println("请输入一个数学计算表达式：");
         String str = console.next();
-
+        String rege = "([0-9]\\d*\\.?\\d*\\+[0-9]\\d*\\.?\\d*)|([0-9]\\d*\\.?\\d*\\-[0-9]\\d*\\.?\\d*)|([0-9]\\d*\\.?\\d*\\*[0-9]\\d*\\.?\\d*)|([0-9]\\d*\\.?\\d*\\/[0-9]\\d*\\.?\\d*)";
+        String rege1 = "(\\-)|(\\*)|(\\+)|(\\/)";
+        if(str.matches(rege)){
+            String[] str1 = str.split(rege1);
+            double i = Double.parseDouble(str1[0]);
+            double j = Double.parseDouble(str1[1]);
+            System.out.print(str+"=");
+            for (int k = 0; k < str.length(); k++) {
+                int flag = str.charAt(k);
+                switch(flag){
+                    case '+' :
+                        System.out.println(i+j);
+                        break;
+                    case '-' :
+                        System.out.println(i-j);
+                        break;
+                    case '*' :
+                        System.out.println(i*j);
+                        break;
+                    case '/' :
+                        System.out.println(i/j);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }else{
+            System.out.println("不是数学计算表达式！");
+        }
 
     }
 }
